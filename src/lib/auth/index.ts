121d572d68b0a09223f0ec191b618894e5740896
makcimerrr <maximedubs@proton.me>
2026-02-15 16:57:51 +0100

    refactor: update StackServerApp configuration with projectId and baseUrl for browser/server contexts

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -6,16 +6,21 @@ const projectId = process.env.NEXT_PUBLIC_STACK_PROJECT_ID;
 export const isAuthConfigured = !!projectId;
 
 export const stackServerApp: StackServerApp = isAuthConfigured
-  ? new StackServerApp({
-      tokenStore: "nextjs-cookie",
-      urls: {
-        home: "/",
-        signIn: "/handler/sign-in",
-        signUp: "/handler/sign-up",
-        afterSignIn: "/",
-        afterSignUp: "/",
-        signOut: "/handler/sign-out",
-        afterSignOut: "/handler/sign-in",
-      },
+    ? new StackServerApp({
+        projectId,
+        tokenStore: "nextjs-cookie",
+        baseUrl: {
+            browser: "http://localhost:8102",  // navigateur
+            server: "http://stack-auth:8102", // serveur Docker
+        },
+        urls: {
+            home: "/",
+            signIn: "/handler/sign-in",
+            signUp: "/handler/sign-up",
+            afterSignIn: "/",
+            afterSignUp: "/",
+            signOut: "/handler/sign-out",
+            afterSignOut: "/handler/sign-in",
+        },
     })
-  : (null as unknown as StackServerApp);
+    : (null as unknown as StackServerApp);
